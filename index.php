15875354cc65dd2327738c691041d5bda0c6deb7
Xarlog <polska.kamil.polska@interia.pl>
2023-01-31 15:13:14 +0100

    fixed typo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Wypożyczalnia DVD</title>
 </head>
-<body style="background-color:#112,color:#fff">
+<body style="background-color:#112;color:#fff">
     <h1>Witaj świecie!</h1>
     <br>
     <p>Maciej</p>
