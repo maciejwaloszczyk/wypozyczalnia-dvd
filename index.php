6ff7506cad79284769546abf2c1ea1ac632047cb
Xarlog <101275079+Xarlog@users.noreply.github.com>
2023-02-07 16:13:22 +0100

    changed header

--- index.php
+++ index.php
@@ -14,20 +14,7 @@
     </head>
     <body>
         <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-info">
-            <div class="container px-5">
-                <a class="navbar-brand" href="#!">Wypożyczalnia DVD</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Strona główna</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Najnowsze</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Kontakt</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Profil</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?php include "header.php" ?>
         <!-- Page Content-->
         <div class="container px-4 px-lg-5">
             <!-- Heading Row-->
