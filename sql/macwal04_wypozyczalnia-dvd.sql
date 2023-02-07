d3461980410cd4820b044add823625485111998e
Maciej Waloszczyk <maciejwaloszczyk04@gmail.com>
2023-02-07 15:29:10 +0100

    add database scheme

---
+++ sql/macwal04_wypozyczalnia-dvd.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Czas generowania: 07 Lut 2023, 15:28
+-- Wersja serwera: 10.3.38-MariaDB
+-- Wersja PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `macwal04_wypozyczalnia-dvd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `rental_data`
+--
+
+CREATE TABLE `rental_data` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_film` int(11) NOT NULL,
+  `isReturned` tinyint(1) NOT NULL,
+  `date_of_return` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `privileges` varchar(5) NOT NULL,
+  `is_banned` tinyint(1) NOT NULL,
+  `is_archived` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `privileges`, `is_banned`, `is_archived`) VALUES
+(1, 'administrator', 'admin1', 'admin@kns.pl', 'ADMIN', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL,
+  `genre` varchar(30) NOT NULL,
+  `releaseYear` date NOT NULL,
+  `director` varchar(60) NOT NULL,
+  `photoDirectiory` varchar(60) NOT NULL,
+  `description` longtext NOT NULL,
+  `dateAdded` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `rental_data`
+--
+ALTER TABLE `rental_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `rental_data`
+--
+ALTER TABLE `rental_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
