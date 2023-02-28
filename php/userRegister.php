e8ae97ae644603d3b3e8a7939c7d3622645d1bb9
Xarlog <polska.kamil.polska@interia.pl>
2023-02-28 14:18:14 +0100

    Closed database_connection

--- php/userRegister.php
+++ php/userRegister.php
@@ -10,11 +10,13 @@
         var_dump($ifExists -> fetch_assoc());
         if (($ifExists -> fetch_assoc()) != NULL)
         {
+            $database_connection->close();
             header("Location: /wypozyczalnia-dvd/register.php?userLoginError=true");
         }
         else
         {
             $result = $database_connection -> query("INSERT INTO users VALUES (NULL,'$InputLogin1', '$InputPassword1', '$InputEmail1', '$privileges', 0, 0)");
+            $database_connection->close();
             header("Location: /wypozyczalnia-dvd/index.php");
         }
     ?>
