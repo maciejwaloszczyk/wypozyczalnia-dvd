9457687890ca5c86cd120ae4188362108e8dd239
Xarlog <polska.kamil.polska@interia.pl>
2023-02-28 15:54:46 +0100

    Created file deleteMovie.php

---
+++ pages/deleteMovie.php
@@ -0,0 +1,19 @@
+<?php include '../php/creds.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete Movie</title>
+</head>
+<body>
+<?php
+    session_start();
+    if($_SESSION["privileges"]!="ADMIN")
+    {
+        header("Location: /wypozyczalnia-dvd/index.php");
+    }
+    ?>
+</body>
+</html>
