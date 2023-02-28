96f1bc16833856bb87953da4529666bacb02aef5
Krzysztof Woliński <krzysztofwolinski04@gmail.com>
2023-02-28 14:13:25 +0100

    KNS-39 - O nas

--- pages/about.php
+++ pages/about.php
@@ -93,12 +93,12 @@
             </div>
         </div>
         <!-- Footer-->
-        <footer class="py-5 bg-dark">
+        <footer class="py-5 bg-dark navbar fixed-bottom">
             <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">KNS Web Services &copy; Wypożyczalnia DVD 2023</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+        <script src="js/scripts.js"></script>  
     </body>
 </html>
