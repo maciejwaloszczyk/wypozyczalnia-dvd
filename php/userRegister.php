de01673e0b0f8673e4bbbe49431a813409641678
Maciej Waloszczyk <maciejwaloszczyk04@gmail.com>
2023-03-04 20:31:24 +0100

    mailing add

--- php/userRegister.php
+++ php/userRegister.php
@@ -29,9 +29,31 @@
         else
         {
             $result = $database_connection -> query("INSERT INTO users VALUES (NULL,'$InputLogin1', '$InputPassword1', '$InputEmail1', '$privileges', 0, 0, 0, '$activation_code')");
-            //session_start();
             $_SESSION["user"]=$database_connection->insert_id;
             $database_connection->close();
+
+            $from = 'noreply@macwal04.smarthost.pl';
+            $fromName = 'KNSRent - najlepsza wypożyczalnia DVD!'; 
+            
+            $subject = "Potwierdzenie rejestracji - KNSRent";
+        
+            $link = 'http://www.macwal04.smarthost.pl/wypozyczalnia-dvd/php/userActivate.php?a=' . $activation_code;
+        
+            $htmlContent = file_get_contents("../assets/mailTemplate/mailTemplate_part1.html") . $link . file_get_contents("../assets/mailTemplate/mailTemplate_part2.html") . $link . file_get_contents("../assets/mailTemplate/mailTemplate_part3.html") . $link . file_get_contents("../assets/mailTemplate/mailTemplate_part4.html");
+            
+            // Set content-type header for sending HTML email 
+            $headers = "MIME-Version: 1.0" . "\r\n"; 
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+            
+            // Additional headers 
+            $headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n"; 
+            
+            // Send email
+            if(mail($InputEmail1, $subject, $htmlContent, $headers)){ 
+                
+            }else{ 
+               echo '<script>alert("Wystąpił błąd w wysłaniu kodu aktywacji! Zarejestruj się ponownie!")</script>'; 
+            }
             header("Location: /wypozyczalnia-dvd/pages/login.php?newUser=true&bref=/wypozyczalnia-dvd/index.php");
         }
     ?>
