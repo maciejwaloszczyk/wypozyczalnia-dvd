ca096156d7bd9702ab4032deb0274580dd29ab0f
Xarlog <polska.kamil.polska@interia.pl>
2023-05-16 14:51:32 +0200

    Fixed a typo

--- index.php
+++ index.php
@@ -112,7 +112,7 @@
                                                     <div class="modal-dialog">
                                                         <div class="modal-content">
                                                         <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Czy na pewno chcesz wyporzyczyć ten film?;</h5>
+                                                            <h5 class="modal-title" id="exampleModalLabel">Czy na pewno chcesz wyporzyczyć ten film?</h5>
                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                         </div>
                                                         <div class="modal-body">
@@ -202,7 +202,7 @@
                                                     <div class="modal-dialog">
                                                         <div class="modal-content">
                                                         <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Czy na pewno chcesz wyporzyczyć ten film?;</h5>
+                                                            <h5 class="modal-title" id="exampleModalLabel">Czy na pewno chcesz wyporzyczyć ten film?</h5>
                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                         </div>
                                                         <div class="modal-body">
